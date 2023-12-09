6781a997654e2da3823a80fe5bb09f496794cc4b
toliadriapak <toliadriapak@gmail.com>
2023-12-09 18:13:28 +0200

    Full upgrade

--- TRIANGLES-AREAS/Main-Triangle.cpp
+++ TRIANGLES-AREAS/Main-Triangle.cpp
@@ -2,6 +2,17 @@
 
 #include <cmath>
 
+#include "Main-Triangle.h"
+#include <iostream>
+#include <Windows.h>
+#include <string>
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+#include <cmath>
+#include <Windows.h>
+
 using namespace System;
 using namespace System::Windows::Forms;
 
@@ -10,6 +21,10 @@ ref class Triangle abstract
 {
 public:
     virtual double CalculateArea() abstract;
+    virtual double CalculateInscribedRadius() abstract;
+    virtual double CalculateCircumscribedRadius() abstract;
+    virtual double CalculateMedianLength() abstract;
+    virtual double CalculateParallelMedian() abstract;
 };
 
 // Concrete implementation based on three vertices
@@ -26,6 +41,53 @@ public:
     virtual double CalculateArea() override
     {
         return 0.5 * Math::Abs(x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2));
+    }
+
+    // Implementation of CalculateInscribedRadius for VertexTriangle
+    virtual double CalculateInscribedRadius() override
+    {
+        double area = CalculateArea();
+        double s = CalculateSemiperimeter();
+        return area / s;
+    }
+
+    // Implementation of CalculateCircumscribedRadius for VertexTriangle
+    virtual double CalculateCircumscribedRadius() override
+    {
+        double a = CalculateSideLength(x2, y2, x3, y3);
+        double b = CalculateSideLength(x1, y1, x3, y3);
+        double c = CalculateSideLength(x1, y1, x2, y2);
+        return (a * b * c) / (4 * CalculateArea());
+    }
+
+    // Implementation of CalculateMedianLength for VertexTriangle
+    virtual double CalculateMedianLength() override
+    {
+        // Implement the calculation of the median length for VertexTriangle
+        // You can replace this with the actual formula
+        return 0.0;
+    }
+
+    // Implementation of CalculateParallelMedian for VertexTriangle
+    virtual double CalculateParallelMedian() override
+    {
+        // Implement the calculation of the parallel median for VertexTriangle
+        // You can replace this with the actual formula
+        return 0.0;
+    }
+
+private:
+    double CalculateSemiperimeter()
+    {
+        double a = CalculateSideLength(x2, y2, x3, y3);
+        double b = CalculateSideLength(x1, y1, x3, y3);
+        double c = CalculateSideLength(x1, y1, x2, y2);
+        return (a + b + c) / 2.0;
+    }
+
+    double CalculateSideLength(double x1, double y1, double x2, double y2)
+    {
+        return Math::Sqrt(Math::Pow(x2 - x1, 2) + Math::Pow(y2 - y1, 2));
     }
 };
 
@@ -45,6 +107,45 @@ public:
         double s = (side1 + side2 + side3) / 2.0;
         return Math::Sqrt(s * (s - side1) * (s - side2) * (s - side3));
     }
+
+    // Implementation of CalculateInscribedRadius for SideTriangle
+    virtual double CalculateInscribedRadius() override
+    {
+        double area = CalculateArea();
+        double s = CalculateSemiperimeter();
+        return area / s;
+    }
+
+    // Implementation of CalculateCircumscribedRadius for SideTriangle
+    virtual double CalculateCircumscribedRadius() override
+    {
+        double a = side1;
+        double b = side2;
+        double c = side3;
+        return (a * b * c) / (4 * CalculateArea());
+    }
+
+    // Implementation of CalculateMedianLength for SideTriangle
+    virtual double CalculateMedianLength() override
+    {
+        // Implement the calculation of the median length for SideTriangle
+        // You can replace this with the actual formula
+        return 0.0;
+    }
+
+    // Implementation of CalculateParallelMedian for SideTriangle
+    virtual double CalculateParallelMedian() override
+    {
+        // Implement the calculation of the parallel median for SideTriangle
+        // You can replace this with the actual formula
+        return 0.0;
+    }
+
+private:
+    double CalculateSemiperimeter()
+    {
+        return (side1 + side2 + side3) / 2.0;
+    }
 };
 
 // Windows Forms Application
@@ -53,6 +154,10 @@ ref class TriangleForm : public Form
 private:
     TextBox^ vertexInputs;
     TextBox^ sideInputs;
+    TextBox^ inscribedRadiusOutput;
+    TextBox^ circumscribedRadiusOutput;
+    TextBox^ medianLengthOutput;
+    TextBox^ parallelMedianOutput;
     Button^ calculateVertexButton;
     Button^ calculateSideButton;
 
@@ -77,13 +182,41 @@ private:
         sideInputs->Height = 60;
         sideInputs->Text = "Enter Side Lengths (side1, side2, side3)";
 
+        inscribedRadiusOutput = gcnew TextBox();
+        inscribedRadiusOutput->Multiline = true;
+        inscribedRadiusOutput->Dock = DockStyle::Top;
+        inscribedRadiusOutput->Height = 30;
+        inscribedRadiusOutput->ReadOnly = true;
+        inscribedRadiusOutput->Text = "Inscribed Radius";
+
+        circumscribedRadiusOutput = gcnew TextBox();
+        circumscribedRadiusOutput->Multiline = true;
+        circumscribedRadiusOutput->Dock = DockStyle::Top;
+        circumscribedRadiusOutput->Height = 30;
+        circumscribedRadiusOutput->ReadOnly = true;
+        circumscribedRadiusOutput->Text = "Circumscribed Radius";
+
+        medianLengthOutput = gcnew TextBox();
+        medianLengthOutput->Multiline = true;
+        medianLengthOutput->Dock = DockStyle::Top;
+        medianLengthOutput->Height = 30;
+        medianLengthOutput->ReadOnly = true;
+        medianLengthOutput->Text = "Median Length";
+
+        parallelMedianOutput = gcnew TextBox();
+        parallelMedianOutput->Multiline = true;
+        parallelMedianOutput->Dock = DockStyle::Top;
+        parallelMedianOutput->Height = 30;
+        parallelMedianOutput->ReadOnly = true;
+        parallelMedianOutput->Text = "Parallel Median";
+
         calculateVertexButton = gcnew Button();
-        calculateVertexButton->Text = "Calculate VertexTriangle Area";
+        calculateVertexButton->Text = "Calculate (Vertex)";
         calculateVertexButton->Dock = DockStyle::Top;
         calculateVertexButton->Click += gcnew EventHandler(this, &TriangleForm::CalculateVertexButton_Click);
 
         calculateSideButton = gcnew Button();
-        calculateSideButton->Text = "Calculate SideTriangle Area";
+        calculateSideButton->Text = "Calculate (Side)";
         calculateSideButton->Dock = DockStyle::Top;
         calculateSideButton->Click += gcnew EventHandler(this, &TriangleForm::CalculateSideButton_Click);
 
@@ -91,59 +224,81 @@ private:
         Controls->Add(calculateVertexButton);
         Controls->Add(sideInputs);
         Controls->Add(calculateSideButton);
+        Controls->Add(inscribedRadiusOutput);
+        Controls->Add(circumscribedRadiusOutput);
+        Controls->Add(medianLengthOutput);
+        Controls->Add(parallelMedianOutput);
     }
 
     void CalculateVertexButton_Click(Object^ sender, EventArgs^ e)
     {
-        try
+        array<String^>^ inputs = vertexInputs->Text->Split(',');
+
+        if (inputs->Length == 6)
         {
-            array<String^>^ vertexParams = vertexInputs->Text->Split(',');
-            double x1 = Convert::ToDouble(vertexParams[0]);
-            double y1 = Convert::ToDouble(vertexParams[1]);
-            double x2 = Convert::ToDouble(vertexParams[2]);
-            double y2 = Convert::ToDouble(vertexParams[3]);
-            double x3 = Convert::ToDouble(vertexParams[4]);
-            double y3 = Convert::ToDouble(vertexParams[5]);
+            try
+            {
+                double x1 = Convert::ToDouble(inputs[0]);
+                double y1 = Convert::ToDouble(inputs[1]);
+                double x2 = Convert::ToDouble(inputs[2]);
+                double y2 = Convert::ToDouble(inputs[3]);
+                double x3 = Convert::ToDouble(inputs[4]);
+                double y3 = Convert::ToDouble(inputs[5]);
 
-            VertexTriangle^ vertexTriangle = gcnew VertexTriangle(x1, y1, x2, y2, x3, y3);
-
-            MessageBox::Show(String::Format("Area of VertexTriangle: {0}", vertexTriangle->CalculateArea()),
-                "Triangle Area", MessageBoxButtons::OK, MessageBoxIcon::Information);
+                VertexTriangle^ triangle = gcnew VertexTriangle(x1, y1, x2, y2, x3, y3);
+                inscribedRadiusOutput->Text = "Inscribed Radius: " + triangle->CalculateInscribedRadius();
+                circumscribedRadiusOutput->Text = "Circumscribed Radius: " + triangle->CalculateCircumscribedRadius();
+                medianLengthOutput->Text = "Median Length: " + triangle->CalculateMedianLength();
+                parallelMedianOutput->Text = "Parallel Median: " + triangle->CalculateParallelMedian();
+            }
+            catch (FormatException^)
+            {
+                MessageBox::Show("Invalid input format. Please enter valid numeric values.");
+            }
         }
-        catch (Exception^ ex)
+        else
         {
-            MessageBox::Show(String::Format("Error: {0}", ex->Message), "Error",
-                MessageBoxButtons::OK, MessageBoxIcon::Error);
+            MessageBox::Show("Invalid input format. Please enter six values separated by commas.");
         }
     }
 
     void CalculateSideButton_Click(Object^ sender, EventArgs^ e)
     {
-        try
+        array<String^>^ inputs = sideInputs->Text->Split(',');
+
+        if (inputs->Length == 3)
         {
-            array<String^>^ sideParams = sideInputs->Text->Split(',');
-            double side1 = Convert::ToDouble(sideParams[0]);
-            double side2 = Convert::ToDouble(sideParams[1]);
-            double side3 = Convert::ToDouble(sideParams[2]);
+            try
+            {
+                double side1 = Convert::ToDouble(inputs[0]);
+                double side2 = Convert::ToDouble(inputs[1]);
+                double side3 = Convert::ToDouble(inputs[2]);
 
-            SideTriangle^ sideTriangle = gcnew SideTriangle(side1, side2, side3);
-
-            MessageBox::Show(String::Format("Area of SideTriangle: {0}", sideTriangle->CalculateArea()),
-                "Triangle Area", MessageBoxButtons::OK, MessageBoxIcon::Information);
+                SideTriangle^ triangle = gcnew SideTriangle(side1, side2, side3);
+                inscribedRadiusOutput->Text = "Inscribed Radius: " + triangle->CalculateInscribedRadius();
+                circumscribedRadiusOutput->Text = "Circumscribed Radius: " + triangle->CalculateCircumscribedRadius();
+                medianLengthOutput->Text = "Median Length: " + triangle->CalculateMedianLength();
+                parallelMedianOutput->Text = "Parallel Median: " + triangle->CalculateParallelMedian();
+            }
+            catch (FormatException^)
+            {
+                MessageBox::Show("Invalid input format. Please enter valid numeric values.");
+            }
         }
-        catch (Exception^ ex)
+        else
         {
-            MessageBox::Show(String::Format("Error: {0}", ex->Message), "Error",
-                MessageBoxButtons::OK, MessageBoxIcon::Error);
+            MessageBox::Show("Invalid input format. Please enter three values separated by commas.");
         }
     }
 };
 
+// Entry point for the application
 int main(array<System::String^>^ args)
 {
     Application::EnableVisualStyles();
     Application::SetCompatibleTextRenderingDefault(false);
 
+    // Create and run the form
     Application::Run(gcnew TriangleForm());
 
     return 0;
